16fc5234e6cac90a9903475e59bce944604b663a
aeris <aeris@Perot-19-Laptop>
2019-02-17 23:41:20 -0700

    Third Version: getHistory method returns history of all actions as a String. Most of all methods and documentations updated.

--- Working Grounds/src/cse360assign2/Assignment2.java
+++ Working Grounds/src/cse360assign2/Assignment2.java
@@ -1,5 +1,96 @@
+//Name: Perot Nguyen
+//CSE 360 M-3:10
+//Assignment: 2
+// This is not a file header
 package cse360assign2;
+import java.util.*;
 
 public class Assignment2 {
+	/** 	
+	 * @author Perot Nguyen, CSE360 M-3:10, Class ID: 508, Assignment 2
+	 * @version "%I%, %G%"
+	 * 
+	 * Assignment2 is the Main file which is shaped as a menu 
+	 * <p>
+	 */
+	public static void main (String[] args) {
+		Scanner scan = new Scanner(System.in);
+		Calculator calc = new Calculator();
+		String check; 
+		int value = 0;
+		try
+		{		
+			do // do actions from the menu repeatedly until user wants to quit (Q)
+			{
+				printMenu();     // print out menu
+				check= scan.nextLine().toUpperCase(); 
+				while(!(check.matches("A|S|M|D|G|T|Q")) || !(check.length() == 1)) 
+				{
+					System.out.println("-----Invalid input, try again.-----");
+					check=scan.nextLine().toUpperCase();
+				}	// Checks to see if user input is able to be used in the menu.
+
+				switch (check) { // do actions based on user's correct input
+				case "A": 	// addition to value
+					System.out.println("\nEnter an integer value to be added to: "+ calc.getTotal());
+					value = scan.nextInt();
+					calc.add(value);
+					scan.nextLine();	// absorb any remaining elements
+					
+					break;
+				case "S":	//subtraction to value
+					System.out.println("\nEnter an integer value to be subtracted from: "+ calc.getTotal());
+					value = scan.nextInt();
+					calc.subtract(value);
+					scan.nextLine();	// absorb any remaining elements
+					break;
+				case "M":	//multiplying the value
+					System.out.println("\nEnter an integer value to be multiplied with: "+ calc.getTotal());
+					value = scan.nextInt();
+					calc.multiply(value);
+					scan.nextLine();	// absorb any remaining elements
+					break;
+				case "D":
+					System.out.println("\nEnter an integer value to be divide the value: "+ calc.getTotal());
+					value = scan.nextInt();
+					calc.divide(value);
+					scan.nextLine();	// absorb any remaining elements
+					break;
+				case "G":
+					System.out.println("History:\n" + calc.getHistory());
+					break;
+				case "T":
+					System.out.println("Current Value: " + calc.getTotal());
+					break;
+				case "Q":	// quit the program
+					break;
+				}
+			}while(!(check.toUpperCase().matches("Q")));			
+		} // try the code and throw any errors
+		catch (Exception exception)
+		{
+			System.out.println("Exception Error Occurred: " + exception);
+			// catch any thrown errors and print out an error message*/
+			
+		}
+		scan.close(); // close Scanner
+	}
 	
+	
+	/*
+	 * printMenu() prints out a string value that creates structure to the menu interface.
+	 */
+	public static void printMenu()
+	{
+		System.out.print("\nAscending List: Enter an option\n" +
+				"Choice\t\tAction\n" +
+				"------\t\t------\n" +
+				"A\t\tAdd Integer\n" +
+				"S\t\tSubtract Integer\n" +
+				"M\t\tMultiply Integer\n" +
+				"D\t\tDivide Integer\n" +
+				"G\t\tGet Action History\n" +
+				"T\t\tGet Current Total\n" +
+				"Q\t\tQuit\n");
+	}
 }
--- Working Grounds/src/cse360assign2/Calculator.java
+++ Working Grounds/src/cse360assign2/Calculator.java
@@ -6,33 +6,88 @@ package cse360assign2;
 
 public class Calculator {
 /**
- * Calculator class
+ * Calculator class manipulates an integer variable which was initialized to 0. 
+ * User can decide to add, subtract, multiply, or divide an integer value onto 
+ * the integer variable that was initialized. Each action is recorded into a 
+ * getHistory function which when called, will print a string of the actions 
+ * dealt to the variable.
  * <p>
+ * This assignment is also suppose to help the programmer use applications such 
+ * as GitHub or BitBucket as a way to push and pull code from a repository off 
+ * a local file, cloud server, or the Internet. Grading will be based off of 
+ * the coding sent as a .java file on Blackboard as well as the repository which
+ * will be linked with the access information in the comments 
+ * for the assignment submittal. 
+ * <p>
+ * Any assistance required with the 
+ * process using GitHub should be directed to this tutorial by Dr. Brian Fraser: 
+ * https://www.youtube.com/watch?v=HuhJ22oHyfc
+ * 
  * @author Perot Nguyen, CSE360 M-3:10, Class ID: 508, Assignment 1
- * @version "%U%, %G%"
+ * @version %U%, %G%
  */
-	private int total;
+	private int total; 				// Variable being manipulated by the user
+	private String history = ""; 	// String variable for getHistory
 	
+	/**
+	 * Base Constructor: initializes total set to 0. Included for clarity.
+	 */
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
 	}
 	
+	/**
+	 * getTotal Method returns the value located in private variable 'total'.
+	 * total's value may be manipulated from user interactions.
+	 *
+	 * @return total - Integer variable that will return its last value before
+	 * getTotal was ran 
+	 */
 	public int getTotal () {
 		return total;
 	}
 	
+	/**
+	 * add Method does addition onto the 'total' variable 
+	 * with its integer parameter 'value'. 
+	 * 
+	 * @param value - Integer variable that will be added to the total value
+	 */
 	public void add (int value) {
 		total = total + value;
+		history += "+ " + value + " ";
 	}
 	
+	/**
+	 * subtract Method does subtraction onto the 'total' variable 
+	 * with its integer parameter 'value'. 
+	 * 
+	 * @param value - Integer variable that will be subtracted to 
+	 * the total value
+	 */
 	public void subtract (int value) {
 		total = total - value;
+		history += "- " + value + " ";
 	}
 	
+	/**
+	 * multiply Method does multiplication onto the 'total' variable 
+	 * with its integer parameter 'value'. 
+	 * 
+	 * @param value - Integer variable that will be multiplied onto
+	 * the total value
+	 */
 	public void multiply (int value) {
 		total = total * value;
+		history += "* " + value + " ";
 	}
 	
+	/**
+	 * divide Method does division onto the 'total' variable 
+	 * with its integer parameter 'value'. 
+	 * 
+	 * @param value - Integer variable that will divide the total value
+	 */
 	public void divide (int value) {
 		if(!(value==0))
 		{
@@ -42,9 +97,19 @@ public class Calculator {
 		{
 			total = 0;
 		}
+		history += "/ " + value + " ";
 	}
 	
+	/**
+	 * getHistory Method returns all actions done to the integer  
+	 * variable 'total' by using the string variable 'history' which was 
+	 * modified after each action. A string value is returned from this method
+	 * with the various actions recorded.
+	 * @return "\n0" + history - a base string which goes to a new line and
+	 * stores the value '0', and the string variable 'history' which had
+	 * recorded all actions done to the total value by the user's choices.
+	 */
 	public String getHistory () {
-		return "";
+		return "\n0 " + history;
 	}
 }
